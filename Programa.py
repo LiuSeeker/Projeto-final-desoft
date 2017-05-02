173f944a364a5b07406ccf48ea85ce5184fb4c82
LiuSeeker <vitor.liu@gmail.com>
2017-05-02 14:57:11 -0300

    Add troll

--- Programa.py
+++ Programa.py
@@ -1,4 +1,40 @@
 import pyglet
+from random import randint
+
+class Window(pyglet.window.Window):
+
+    def __init__ (self):
+        super(Window, self).__init__(1200,600,caption="RPG",resizable=True,anchor_x="center",anchor_y="center")
+        self.char = Humano("Citizen.png", 400, 300)
+        self.snout = Troll("Troll.png", 100, 100)
+
+        pyglet.clock.schedule_interval(self.update, 0.000000001)
+
+    def update(self, dt):
+        self.clear()
+        self.char.draw()
+        self.snout.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.UP:
+            self.char.addy = 2
+        elif symbol == pyglet.window.key.DOWN:
+            self.char.addy = -2
+        elif symbol == pyglet.window.key.LEFT:
+             self.char.addx = -2
+        elif symbol == pyglet.window.key.RIGHT:
+            self.char.addx = 2
+
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == pyglet.window.key.UP:
+            self.char.addy = 0
+        elif symbol == pyglet.window.key.DOWN:
+            self.char.addy = 0
+        elif symbol == pyglet.window.key.LEFT:
+            self.char.addx = 0
+        elif symbol == pyglet.window.key.RIGHT:
+            self.char.addx = 0
 
 class Humano():
 
@@ -14,39 +50,19 @@ class Humano():
         self.y += self.addy
         self.image.blit(self.x,self.y)
 
+class Troll():
 
-class Window(pyglet.window.Window):
+    def __init__(self,nome, x, y):
+        self.x = x
+        self.y = y
+        self.addx = 0
+        self.addy = 0
+        self.image = pyglet.resource.image(nome)
 
-    def __init__ (self):
-        super(Window, self).__init__(800,600,caption="RPG")
-        self.char = Humano("Ciizen.png", 400, 300)
-
-        pyglet.clock.schedule_interval(self.update, 0.001)
-
-    def update(self, dt):
-        self.clear()
-        self.char.draw()
-
-    def on_key_press(self, symbol, modifiers):
-        if symbol == pyglet.window.key.UP:
-            self.char.addy = 5
-        elif symbol == pyglet.window.key.DOWN:
-            self.char.addy = -5
-        elif symbol == pyglet.window.key.LEFT:
-             mself.char.addx = -5
-        elif symbol == pyglet.window.key.RIGHT:
-            self.char.addx = 5
-        print(self.char.addy, self.char.addx)
-
-    def on_key_release(self, symbol, modifiers):
-        if symbol == pyglet.window.key.UP:
-            self.char.addy = 0
-        elif symbol == pyglet.window.key.DOWN:
-            self.char.addy = 0
-        elif symbol == pyglet.window.key.LEFT:
-            self.char.addx = 0
-        elif symbol == pyglet.window.key.RIGHT:
-            self.char.addx = 0
+    def draw(self):
+        self.x += self.addx
+        self.y += self.addy
+        self.image.blit(self.x,self.y)
         
 def main():
     window = Window()
@@ -54,4 +70,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
