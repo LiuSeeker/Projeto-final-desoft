4343b787e5a42cd0225e2fe54eadec62d4682e4c
LiuSeeker <vitor.liu@gmail.com>
2017-05-02 21:58:37 -0300

    Video 2 concluido

--- Main.py
+++ Main.py
@@ -12,9 +12,20 @@ WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
-BLACK = (0,0,255)
+BLUE = (0,0,255)
 LIGHTGREY = (50,50,50)
 DARKGREY = (100,100,100)
+
+#Classe do jogador
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #teste com um quadrado verde
+        self.image = pygame.Surface((50,50)) #tamanho
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect() #ponto de referência da imagem selecionada 
+        self.rect.center = (WIDTH/2, HEIGHT/2) #posição do centro do sprite na tela
 
 #inicia pygame e cria janela
 pygame.init()
@@ -22,6 +33,11 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("RPG")
 clock = pygame.time.Clock()
+player = Player()
+
+#cria um grupo de sprites
+grupo_sprites = pygame.sprite.Group()
+grupo_sprites.add(player) #adiciona o player no grupo de sprites
 
 #loop principal
 running = True
@@ -34,7 +50,8 @@ while running:
             running = False
 
     #espaço para "desenho"
-    screen.fill(DARKGREY)
+    screen.fill(DARKGREY) #pinta o fundo de darkgrey
+    grupo_sprites.draw(screen) #pinta a grupo_sprites
 
     #aparece as imagens
     pygame.display.flip()
