e1158e04b6755fe2a07633c8743000216da39eac
LiuSeeker <vitor.liu@gmail.com>
2017-05-02 22:40:04 -0300

    Add classe Monstro

--- Main.py
+++ Main.py
@@ -3,26 +3,35 @@ import random
 
 #propriedades da janela
 WIDTH = 1280
-HEIGHT = 720
+HEIGHT = 736
 FPS = 60
 
 #definição das cores e o nome das variaveis
 #(R,G,B)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLACK = (0,0,255)
+RED = (200,0,0)
+GREEN = (0,200,0)
+BLUE = (0,0,200)
 LIGHTGREY = (50,50,50)
 DARKGREY = (100,100,100)
 
+#cria a classe jogador
 class Jogador(pygame.sprite.Sprite):
-	def __init__ (self):
+	def __init__ (self,x,y):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface((50,50))
+		self.image = pygame.Surface((50,50)) #tamanho da imagem
 		self.image.fill(RED)
-		self.rect = self.image.get_rect()
-		self.rect.center = (WIDTH / 2, HEIGHT / 2)
+		self.rect = self.image.get_rect() 
+		self.rect.center = (x,y) #define o centro da imgagem como referência para desenhar
+
+class Monstro(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((40,40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
 
 #inicia pygame e cria janela
 pygame.init()
@@ -31,9 +40,13 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("RPG")
 clock = pygame.time.Clock()
 
-sprites = pygame.sprite.Group()
-jogador1 = Jogador()
-sprites.add(jogador1)
+
+#grupo de sprites
+sprites = pygame.sprite.Group() #define o nome do grupo
+jogador1 = Jogador(WIDTH/2, HEIGHT/2)
+monstro1 = Monstro(50,50)
+sprites.add(jogador1) #adiciona o jogador1 no grupo
+sprites.add(monstro1) #adiciona o monstro1 no grupo
 
 
 #loop principal
