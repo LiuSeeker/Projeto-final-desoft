cd34ce2e8f83ed79b158e789553376f2f2daf41b
LiuSeeker <vitor.liu@gmail.com>
2017-05-04 15:34:19 -0300

    Add os tiles

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ import pygame
 import random
 
 #propriedades da janela
-WIDTH = 1280
-HEIGHT = 736
-FPS = 60
+WIDTH = 32*35
+HEIGHT = 32*21
+FPS = 15
 
 #definição das cores e o nome das variaveis
 #(R,G,B)
@@ -16,11 +16,15 @@ BLUE = (0,0,200)
 LIGHTGREY = (50,50,50)
 DARKGREY = (100,100,100)
 
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
 #cria a classe jogador
 class Jogador(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,50)) #tamanho da imagem
+        self.image = pygame.Surface((TILESIZE,TILESIZE)) #tamanho da imagem
         self.image.fill(GREEN)
         self.rect = self.image.get_rect() 
         self.rect.center = (x,y) #define o centro da imgagem como referência para desenhar
@@ -28,19 +32,18 @@ class Jogador(pygame.sprite.Sprite):
         self.speedy = 0
 
     def update(self):
-
         #Movimento do player
         self.speedx = 0
         self.speedy = 0
         Teclas_pressionadas = pygame.key.get_pressed() #analisa todas as teclas pressionadas
-        if Teclas_pressionadas[pygame.K_LEFT]: 
-            self.speedx = -5
-        if Teclas_pressionadas[pygame.K_RIGHT]:
-            self.speedx = 5
+        if Teclas_pressionadas[pygame.K_LEFT]:
+            self.speedx = -1 * TILESIZE #anda 1 quadrado
+        elif Teclas_pressionadas[pygame.K_RIGHT]:
+            self.speedx = 1 * TILESIZE #anda 1 quadrado
         if Teclas_pressionadas[pygame.K_UP]:
-            self.speedy = -5
-        if Teclas_pressionadas[pygame.K_DOWN]:
-            self.speedy = 5
+            self.speedy = -1 * TILESIZE #anda 1 quadrado
+        elif Teclas_pressionadas[pygame.K_DOWN]:
+            self.speedy = 1 * TILESIZE #anda 1 quadrado
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.right > WIDTH:
@@ -55,10 +58,26 @@ class Jogador(pygame.sprite.Sprite):
 class Monstro(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((40,40))
+        self.image = pygame.Surface((TILESIZE,TILESIZE)) #tamanho da imagem
         self.image.fill(RED)
+        self.rect = self.image.get_rect() 
+        self.x = x
+        self.y = y
+        self.rect.x = x*TILESIZE
+        self.rect.y = y*TILESIZE
+
+class Parede(pygame.sprite.Sprite):
+    def __init__(self, x, y, cor):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((TILESIZE,TILESIZE))
+        self.image.fill(cor)
         self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
+        self.x = x
+        self.y = y
+        self.rect.x = x*TILESIZE
+        self.rect.y = y*TILESIZE
+
+
 
 #inicia pygame e cria janela
 pygame.init()
@@ -70,10 +89,13 @@ clock = pygame.time.Clock()
 
 #grupo de sprites
 sprites = pygame.sprite.Group() #define o nome do grupo
+paredes = pygame.sprite.Group()
 jogador1 = Jogador(WIDTH/2, HEIGHT/2) #argumentos definem a posição do objeto
-monstro1 = Monstro(50,50)
-sprites.add(jogador1) #adiciona o jogador1 no grupo
-sprites.add(monstro1) #adiciona o monstro1 no grupo
+monstro1 = Monstro(2,2)
+monstro2 = Monstro(10,10)
+sprites.add(jogador1)
+sprites.add(monstro1)
+sprites.add(monstro2)
 
 
 #loop principal
@@ -92,6 +114,10 @@ while running:
     screen.fill(DARKGREY)
     sprites.draw(screen)
 
+    for x in range(0, WIDTH, TILESIZE):
+        pygame.draw.line(screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+    for x in range(0, HEIGHT, TILESIZE):
+        pygame.draw.line(screen, LIGHTGREY, (0, x), (WIDTH, x))
 
     #aparece as imagens
     pygame.display.flip()
